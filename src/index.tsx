683b59b209d4b0c19271b84b8e1f122e5cc0cc1f
Andrey Botygin <dron-i90@yandex.ru>
2021-06-18 19:21:45 +0300

    final

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/todo-redux-ts">
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
